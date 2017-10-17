b703dd7765fc3e74fd9617560b5587359109e5f8
Alex Fenton <fenton@dzhw.eu>
2017-10-17 12:22:24 +0200

    Note source of min/max warnings on MingW64, Fix later

--- ext/wapiti/tools.h
+++ ext/wapiti/tools.h
@@ -35,6 +35,8 @@
 #define unused(v) ((void)(v))
 #define none ((uint64_t)-1)
 
+// This is compiling with warnings on MingW64, because ruby provides
+// equivalent definitions.
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) < (b) ? (b) : (a))
 
